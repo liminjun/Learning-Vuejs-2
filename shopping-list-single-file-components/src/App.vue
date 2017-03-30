513976bffc72a48fb4f55230025c48f60bbaebb1
liminjun <v-leeli@hotmail.com>
2017-03-30 21:24:08 +0800

    feat:add shopping list using single file component.

--- shopping-list-single-file-components/src/App.vue
+++ shopping-list-single-file-components/src/App.vue
@@ -2,16 +2,40 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <add-item-component></add-item-component>
+    <items-component :items="items"></items-component>
+    <change-title-component></change-title-component>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
 
+
+import AddItemComponent from './components/AddItemComponent' 
+  import ItemsComponent from './components/ItemsComponent' 
+  import ChangeTitleComponent from './components/ChangeTitleComponent' 
+
 export default {
   name: 'app',
   components: {
-    Hello
+    AddItemComponent, 
+      ItemsComponent, 
+      ChangeTitleComponent 
+  },
+  data() {
+    return {
+      items: [{ text: 'Bananas', checked: true }, 
+                { text: 'Apples', checked: false }] 
+    }
+  },
+  methods:{
+    addItem(text){
+      this.items.push({
+        text:text,
+        checked:false
+      });
+    }
   }
 }
 </script>
