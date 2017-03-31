55a8220836510f524267f8cfa29e3ce7d1ac4d70
liminjun <v-leeli@hotmail.com>
2017-03-31 14:14:01 +0800

    feat:add codes using vuex to store the state and mutations.

---
+++ simple-store/src/components/ChangeGreetingsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<input type="text" v-model="msg" @keyup="changeMsg">
+</template>
+<script>
+    export default {
+        computed:{
+            msg(){
+                return this.$store.state.msg
+            }
+        },
+        methods:{
+            changeMsg(event){
+                this.$store.commit('changeMessage',event.target.value)
+            }
+        }
+    }
+</script>
