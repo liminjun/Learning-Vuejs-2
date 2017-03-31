cfc9c1fdf04fc062e83947aa7545bb22229101a1
liminjun <v-leeli@hotmail.com>
2017-03-31 22:23:10 +0800

    fix:resolve conflicts

--- simple-store/src/components/ChangeGreetingsComponent.vue
+++ simple-store/src/components/ChangeGreetingsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-<<<<<<< HEAD
+
 <input type="text" v-model="msg" @keyup="changeMsg($event.target.value)" @keyup.enter="incrementCounter">
 
 
@@ -13,21 +13,6 @@ import { mapActions } from 'vuex'
             msg:'getMessage'
         }),
         methods:  mapActions(['changeMessage', 'incrementCounter']) 
-=======
-<input type="text" v-model="msg" @keyup="changeMsg">
-</template>
-<script>
-    export default {
-        computed:{
-            msg(){
-                return this.$store.state.msg
-            }
-        },
-        methods:{
-            changeMsg(event){
-                this.$store.commit('changeMessage',event.target.value)
-            }
-        }
->>>>>>> 55a8220836510f524267f8cfa29e3ce7d1ac4d70
+
     }
 </script>
--- simple-store/src/components/ShowGreetingsComponent.vue
+++ simple-store/src/components/ShowGreetingsComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>{{msg}}</h1>
-<<<<<<< HEAD
+
     <div>the message was changed {{counter}} times</div>
 </template>
 <script>
@@ -11,15 +11,6 @@ import {mapGetters} from 'vuex'
             msg:'getMessage',
             counter:'getCounter'
         })
-=======
-</template>
-<script>
-    export default {
-        computed:{
-            msg(){
-                return this.$store.state.msg
-            }
-        }
->>>>>>> 55a8220836510f524267f8cfa29e3ce7d1ac4d70
+
     }
 </script>
